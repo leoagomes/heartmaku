48cf35141d0b84a6a6ee451f362a889d24e34411
Leonardo G. <leoagomes@users.noreply.github.com>
2026-01-14 23:40:09 -0300

    add Entity doc comment

--- app/entities/base.rb
+++ app/entities/base.rb
@@ -1,3 +1,9 @@
+# Entities are often copied, and many are spawned and thrown away.
+# For this reason, entities should be kept as small and flat as possible.
+# This way, `dup` will return a full clone.
+#
+# If, for some reason, an entitie uses mutable nested objects, it should override
+# `dup` to also clone that -- some sort of "deep" dup.
 module Entities
   class Base
     include Components::Anchor
